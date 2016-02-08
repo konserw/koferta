db0ac12c5be263d3694c140ee98f2d566d445213
Kamil Strzempowicz <konserw@gmail.com>
2016-02-08 09:28:58 +0100

    smiecie

--- LoadDialog.cpp
+++ LoadDialog.cpp
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
-#include <QProcess>
+
 #include "LoadDialog.h"
 #include "ui_LoadDialog.h"
 #include "OfferSearch.h"
