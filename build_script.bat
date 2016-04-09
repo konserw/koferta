b1fd6cb8cc10bce1739c2c64c9719ec63ee65221
Kamil Strzempowicz <konserw@gmail.com>
2016-04-09 14:21:23 +0200

    fix script

--- build_script.bat
+++ build_script.bat
@@ -1,10 +1,10 @@
 @echo on
 cmake -E make_directory build
-cmake -E chdir build cmake ..
 IF %APPVEYOR_REPO_TAG% EQU true (
 echo %APPVEYOR_REPO_TAG_NAME%
-cmake --build build --config Release -DKOFERTA_VERSION=%APPVEYOR_REPO_TAG_NAME%
+cmake -E chdir build cmake -DKOFERTA_VERSION=%APPVEYOR_REPO_TAG_NAME% ..
 ) ELSE (
-cmake --build build --config Release -DKOFERTA_VERSION=%APPVEYOR_REPO_BRANCH%-%APPVEYOR_BUILD_NUMBER%
 echo %APPVEYOR_REPO_BRANCH%-%APPVEYOR_BUILD_NUMBER%
+cmake -E chdir build cmake -DKOFERTA_VERSION=%APPVEYOR_REPO_BRANCH%-%APPVEYOR_BUILD_NUMBER% ..
 )
+cmake --build build --config Release
