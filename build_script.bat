8069b26250aee57aefbff6c130857ae68a2f8354
Kamil Strzempowicz <konserw@gmail.com>
2016-04-09 14:08:25 +0200

    fix typo

--- build_script.bat
+++ build_script.bat
@@ -3,8 +3,8 @@ cmake -E make_directory build
 cmake -E chdir build cmake ..
 IF %APPVEYOR_REPO_TAG% EQU true (
 echo %APPVEYOR_REPO_TAG_NAME%
-cmake --build build --config Release -DKOFRTA_VERSION=%APPVEYOR_REPO_TAG_NAME%
+cmake --build build --config Release -DKOFERTA_VERSION=%APPVEYOR_REPO_TAG_NAME%
 ) ELSE (
-cmake --build build --config Release -DKOFRTA_VERSION=%APPVEYOR_REPO_BRANCH%-%APPVEYOR_BUILD_NUMBER%
+cmake --build build --config Release -DKOFERTA_VERSION=%APPVEYOR_REPO_BRANCH%-%APPVEYOR_BUILD_NUMBER%
 echo %APPVEYOR_REPO_BRANCH%-%APPVEYOR_BUILD_NUMBER%
 )
