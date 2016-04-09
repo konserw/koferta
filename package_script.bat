051e967157907bb7bfdf8b2c1873aee9d0634ca2
Kamil Strzempowicz <konserw@gmail.com>
2016-04-09 15:39:30 +0200

    copy x86 msvcp dll

--- package_script.bat
+++ package_script.bat
@@ -6,8 +6,8 @@ cp ../build/MainWindow/*.qm ./
 cp ../3rdParty/* ./
 cp ../LICENSE.md ./
 cp ../README.md ./
-cp C:\Windows/System32/msvcp120.dll ./
-cp C:\Windows/System32/msvcr120.dll ./
+cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll" ./
+cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll" ./
 cp C:\Qt/5.5/msvc2013/bin/Qt5Core.dll ./
 cp C:\Qt/5.5/msvc2013/bin/Qt5Gui.dll ./
 cp C:\Qt/5.5/msvc2013/bin/Qt5PrintSupport.dll ./
