fa7eccac7e367bfda0d9a932908937ba7f32f62a
Kamil Strzempowicz <konserw@gmail.com>
2016-04-09 13:50:07 +0200

    use tyag defined appveyor variable

--- build_script.bat
+++ build_script.bat
@@ -1,7 +1,7 @@
 @echo on
 cmake -E make_directory build
 cmake -E chdir build cmake ..
-IF DEFINED %APPVEYOR_REPO_TAG_NAME% (
+IF %APPVEYOR_REPO_TAG% (
 echo %APPVEYOR_REPO_TAG_NAME%
 cmake --build build --config Release -DKOFRTA_VERSION=%APPVEYOR_REPO_TAG_NAME%
 ) ELSE (
