46a778ec512b7689f6a8965c655a4beb0f72986e
Kamil Strzempowicz <konserw@gmail.com>
2016-10-05 18:09:57 +0200

    move all binaries to bin folder in windows build, add Logger to package script

--- package_script.bat
+++ package_script.bat
@@ -1,11 +1,14 @@
 mkdir koferta_x86
 cd koferta_x86
-cp ../build/Release/koferta.exe ./
-cp ../build/MainWindow/Release/mainwindow.dll ./
-cp ../build/MainWindow/*.qm ./
-cp ../3rdParty/* ./
+mkdir bin
+cp ../build/Release/koferta.exe ./bin/
+cp ../build/Logger/Release/logger.dll ./bin/
+cp ../build/MainWindow/Release/mainwindow.dll ./bin/
+cp ../build/MainWindow/*.qm ./bin/
+cp ../3rdParty/* ./bin/
 cp ../LICENSE.md ./
 cp ../README.md ./
+cd bin
 cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll" ./
 cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll" ./
 cp C:\Qt/5.5/msvc2013/bin/Qt5Core.dll ./
@@ -20,5 +23,5 @@ mkdir printsupport
 cp C:\Qt/5.5/msvc2013/plugins/printsupport/windowsprintersupport.dll ./printsupport/
 mkdir sqldrivers
 cp C:\Qt/5.5/msvc2013/plugins/sqldrivers/qsqlmysql.dll ./sqldrivers/
-cd ..
+cd ../..
 7z a kOferta_x86.7z koferta_x86
