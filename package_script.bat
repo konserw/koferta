5df59dd0e8098d0b94470f77c81b4ffa01a84bef
Kamil Strzempowicz <konserw@gmail.com>
2016-10-06 18:09:46 +0200

    switch to qt 5.7 in travis and appveyor due to QFlag::testFlag introduced in Qt5.7

--- package_script.bat
+++ package_script.bat
@@ -11,17 +11,17 @@ cp ../README.md ./
 cd bin
 cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll" ./
 cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll" ./
-cp C:\Qt/5.5/msvc2013/bin/Qt5Core.dll ./
-cp C:\Qt/5.5/msvc2013/bin/Qt5Gui.dll ./
-cp C:\Qt/5.5/msvc2013/bin/Qt5PrintSupport.dll ./
-cp C:\Qt/5.5/msvc2013/bin/Qt5Sql.dll ./
-cp C:\Qt/5.5/msvc2013/bin/Qt5Widgets.dll ./
-cp C:\Qt/5.5/msvc2013/bin/Qt5Network.dll ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5Core.dll ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5Gui.dll ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5PrintSupport.dll ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5Sql.dll ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5Widgets.dll ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5Network.dll ./
 mkdir platforms
-cp C:\Qt/5.5/msvc2013/plugins/platforms/qwindows.dll ./platforms/
+cp C:\Qt/5.7/msvc2013/plugins/platforms/qwindows.dll ./platforms/
 mkdir printsupport
-cp C:\Qt/5.5/msvc2013/plugins/printsupport/windowsprintersupport.dll ./printsupport/
+cp C:\Qt/5.7/msvc2013/plugins/printsupport/windowsprintersupport.dll ./printsupport/
 mkdir sqldrivers
-cp C:\Qt/5.5/msvc2013/plugins/sqldrivers/qsqlmysql.dll ./sqldrivers/
+cp C:\Qt/5.7/msvc2013/plugins/sqldrivers/qsqlmysql.dll ./sqldrivers/
 cd ../..
 7z a kOferta_x86.7z koferta_x86
