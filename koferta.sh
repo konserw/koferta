8abb20ec86caa439ac726b219ee6cb9522505572
Kamil Strzempowicz <konserw@gmail.com>
2018-05-07 19:37:25 +0200

    disable checking key in known hosts

--- koferta.sh
+++ koferta.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir ./build/log
-ssh -M -S ssh.sock -v 46.105.27.6 -p 2292 -l konserw -i ~/.ssh/koferta_rsa -fN -L 13306:46.105.27.6:3306 -o ExitOnForwardFailure=yes localhost >./build/log/ssh.out 2>./build/log/ssh.err 
+ssh -M -S ssh.sock -v 46.105.27.6 -p 2292 -l konserw -i ~/.ssh/koferta_rsa -fN -L 13306:46.105.27.6:3306 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no localhost >./build/log/ssh.out 2>./build/log/ssh.err 
 ssh -S ssh.sock -O check localhost 
 netstat -tulpn |grep 3306
 ./build/koferta
