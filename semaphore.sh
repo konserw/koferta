4e9126a892c34cc9a4204110f39329bc88f94754
Kamil Strzempowicz <konserw@gmail.com>
2018-05-31 13:37:33 +0200

    preparation for tests

--- semaphore.sh
+++ semaphore.sh
@@ -5,7 +5,14 @@ CTEST_OUTPUT_ON_FAILURE=ON
 export CTEST_OUTPUT_ON_FAILURE
 
 cmake -E make_directory build
-cmake -E chdir build cmake -DKOFERTA_VERSION="${BRANCH_NAME}-${SEMAPHORE_BUILD_NUMBER}" -GNinja -DKOFERTA_SQL_USER="${KOFERTA_SQL_USER}" -DKOFERTA_SQL_PWD="${KOFERTA_SQL_PWD}" ..
+cmake -E chdir build cmake \
+    -DKOFERTA_VERSION="${BRANCH_NAME}-${SEMAPHORE_BUILD_NUMBER}" \
+    -GNinja \
+    -DKOFERTA_SQL_USER="${KOFERTA_SQL_USER}" \
+    -DKOFERTA_SQL_PWD="${KOFERTA_SQL_PWD}" \
+    -DDISABLE_UNIT_TESTS=ON \
+    ${VALGRIND_TESTS:+"-DVALGRIND_TESTS=${VALGRIND_TESTS}"} \
+    ..
 cmake --build build
 
 #build/kofertaSteps >/dev/null &
