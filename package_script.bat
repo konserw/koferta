f76e86f78c56d37aee12880fdd09f02f14ce8712
strzempo <strzempo@tlen.pl>
2018-06-08 19:24:44 +0200

    update appveyor.yml to VS2017 and Qt5.11

--- package_script.bat
+++ package_script.bat
@@ -1,5 +1,5 @@
-mkdir koferta_x86
-cd koferta_x86
+mkdir koferta
+cd koferta
 mkdir bin
 cp ../build/Release/koferta.exe ./bin/
 cp ../build/Logger/Release/logger.dll ./bin/
@@ -10,19 +10,19 @@ cp ../LICENSE.md ./
 cp ../README.md ./
 cp ../koferta.bat ./
 cd bin
-cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll" ./
-cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll" ./
-cp C:\Qt/5.7/msvc2013/bin/Qt5Core.dll ./
-cp C:\Qt/5.7/msvc2013/bin/Qt5Gui.dll ./
-cp C:\Qt/5.7/msvc2013/bin/Qt5PrintSupport.dll ./
-cp C:\Qt/5.7/msvc2013/bin/Qt5Sql.dll ./
-cp C:\Qt/5.7/msvc2013/bin/Qt5Widgets.dll ./
-cp C:\Qt/5.7/msvc2013/bin/Qt5Network.dll ./
+REM cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll" ./
+REM cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll" ./
+cp C:\Qt/5.11/msvc2017_64/bin/Qt5Core.dll ./
+cp C:\Qt/5.11/msvc2017_64/bin/Qt5Gui.dll ./
+cp C:\Qt/5.11/msvc2017_64/bin/Qt5PrintSupport.dll ./
+cp C:\Qt/5.11/msvc2017_64/bin/Qt5Sql.dll ./
+cp C:\Qt/5.11/msvc2017_64/bin/Qt5Widgets.dll ./
+cp C:\Qt/5.11/msvc2017_64/bin/Qt5Network.dll ./
 mkdir platforms
-cp C:\Qt/5.7/msvc2013/plugins/platforms/qwindows.dll ./platforms/
+cp C:\Qt/5.11/msvc2017_64/plugins/platforms/qwindows.dll ./platforms/
 mkdir printsupport
-cp C:\Qt/5.7/msvc2013/plugins/printsupport/windowsprintersupport.dll ./printsupport/
+cp C:\Qt/5.11/msvc2017_64/plugins/printsupport/windowsprintersupport.dll ./printsupport/
 mkdir sqldrivers
-cp C:\Qt/5.7/msvc2013/plugins/sqldrivers/qsqlmysql.dll ./sqldrivers/
+cp C:\Qt/5.11/msvc2017_64/plugins/sqldrivers/qsqlmysql.dll ./sqldrivers/
 cd ../..
-7z a kOferta_x86.7z koferta_x86
+7z a kOferta.7z koferta
