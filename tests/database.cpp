c75fa43b9bfdc1514defe098c9255d1fc8b08aa6
Kamil Strzempowicz <konserw@gmail.com>
2018-06-17 18:24:14 +0200

    use QtTest instead of gtest for testing

---
+++ tests/database.cpp
@@ -0,0 +1,21 @@
+#include <QtTest/QtTest>
+#include "../MainWindow/Database.h"
+namespace tests {
+
+class database : public QObject {
+    Q_OBJECT
+private slots:
+    void setup_connection()
+    {
+        QString db_name = QProcessEnvironment::systemEnvironment().value("DATABASE_MYSQL_USERNAME", "test_user");
+        QString db_pass = QProcessEnvironment::systemEnvironment().value("DATABASE_MYSQL_PASSWORD", "test_password");
+        Database::instance()->setupDatabaseConnection("localhost", 3306, "kOferta_test", db_name, db_pass);
+        QVERIFY(Database::instance()->isConnected());
+        Database::instance()->dropConection();
+    }
+};
+
+}
+
+QTEST_MAIN(tests::database)
+#include "database.moc"
