68f7a65c8ffbed0518b2d42126a2bba04222ffb0
Kamil Strzempowicz <konserw@gmail.com>
2018-07-01 22:26:56 +0200

    fix appveyor test

--- tests/DatabaseTest.cpp
+++ tests/DatabaseTest.cpp
@@ -8,7 +8,12 @@ private slots:
     void setup_connection()
     {
         QString db_name = QProcessEnvironment::systemEnvironment().value("DATABASE", "kOferta_test");
-        Database::instance()->setupDatabaseConnection("localhost", 3306, db_name);
+        QString db_user = QProcessEnvironment::systemEnvironment().value("SQL_USER", "");
+        QString db_pass = QProcessEnvironment::systemEnvironment().value("SQL_PASS", "");
+        if(db_user.isEmpty() || db_pass.isEmpty())
+            Database::instance()->setupDatabaseConnection("localhost", 3306, db_name);
+        else
+            Database::instance()->setupDatabaseConnection("localhost", 3306, db_name, db_user, db_pass);
         QVERIFY(Database::instance()->isConnected());
         Database::instance()->dropConection();
     }
